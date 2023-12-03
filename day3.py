4468f1ac11a3517170ef25cc528d050f548f3a33
dkhex <dk-hex@yandex.ru>
2023-12-03 09:52:37 +0300

    Day 3 task 2 solution, ToDo: refactor it

--- day3.py
+++ day3.py
@@ -35,35 +35,57 @@ def get_numbers_and_symbols(row_string):
                 numbers.update({n: number for n in range(start, i)})
                 num = 0
             if s != ".":
-                symbols.append(i)
+                symbols.append((i, s))
     return numbers, symbols
 
 
-def task1(filename):
+def process_rows(lines):
     number_rows = [{}]
     symbol_rows = [[]]
-    with open(filename) as file:
-        for line in file:
-            numbers, symbols = get_numbers_and_symbols(line.replace("\n", "."))
-            number_rows.append(numbers)
-            symbol_rows.append(symbols)
+    for line in lines:
+        numbers, symbols = get_numbers_and_symbols(line.replace("\n", "."))
+        number_rows.append(numbers)
+        symbol_rows.append(symbols)
     number_rows.append({})
     symbol_rows.append([])
+    return number_rows, symbol_rows
+
+
+def task1(filename):
+    with open(filename) as file:
+        number_rows, symbol_rows = process_rows(file)
+
     number_set = set()
     for row_num, row in enumerate(symbol_rows):
-        for symbol_pos in row:
+        for symbol_pos, _ in row:
             for x, y in around_coords(symbol_pos, row_num):
                 if (number := number_rows[y].get(x)) is not None:
                     number_set.add(number)
+
     return sum(map(lambda x: x.value, number_set))
 
 
 def task2(filename):
     with open(filename) as file:
-        ...
+        number_rows, symbol_rows = process_rows(file)
+
+    ratios = []
+    for row_num, row in enumerate(symbol_rows):
+        for symbol_pos, symbol in row:
+            if symbol != "*":
+                continue
+            adjacent = set()
+            for x, y in around_coords(symbol_pos, row_num):
+                if (number := number_rows[y].get(x)) is not None:
+                    adjacent.add(number)
+            if len(adjacent) == 2:
+                a, b = adjacent
+                ratios.append(a.value * b.value)
+
+    return sum(ratios)
 
 
 if __name__ == "__main__":
     filename = "inputs/day3.txt"
     print("Task 1:", task1(filename))
-    # print("Task 2:", task2(filename))
+    print("Task 2:", task2(filename))
