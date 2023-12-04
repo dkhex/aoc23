cb73961823436e1fc50a2415fdc08be77f1ab46d
dkhex <dk-hex@yandex.ru>
2023-12-04 19:33:38 +0300

    Day 4 task 2 solution, ok I guess

--- day4.py
+++ day4.py
@@ -1,16 +1,24 @@
+from collections import defaultdict
+
 from myiters import ChainIterable
+
+
+def get_win_counts(lines):
+    return (
+        ChainIterable(lines)
+            .map(lambda line: line.split(":")[-1].strip())
+            .map(lambda line: line.split("|"))
+            .submap(str.split)
+            .submap(set)
+            .tuple_map(set.intersection)
+            .map(len)
+    )
 
 
 def task1(filename):
     with open(filename) as file:
         return (
-            ChainIterable(file)
-                .map(lambda line: line.split(":")[-1].strip())
-                .map(lambda line: line.split("|"))
-                .submap(str.split)
-                .submap(set)
-                .tuple_map(set.intersection)
-                .map(len)
+            get_win_counts(file)
                 .map(lambda count: (1 << count) // 2)
                 .sum()
         )
@@ -18,10 +26,16 @@ def task1(filename):
 
 def task2(filename):
     with open(filename) as file:
-        ...
+        win_counts = get_win_counts(file).collect()
+
+    copy_counts = defaultdict(lambda: 1)
+    for next_number, wins in enumerate(win_counts, start=1):
+        for card_number in range(next_number, next_number + wins):
+            copy_counts[card_number] += copy_counts[next_number - 1]
+    return sum(copy_counts.values())
 
 
 if __name__ == "__main__":
     filename = "inputs/day4.txt"
     print("Task 1:", task1(filename))
-    # print("Task 2:", task2(filename))
+    print("Task 2:", task2(filename))
