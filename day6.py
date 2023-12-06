8fce8dced8d5b391b553cee446a40414daec5844
dkhex <dk-hex@yandex.ru>
2023-12-06 21:11:38 +0300

    Day 6 task 1 solution, naive algo

---
+++ day6.py
@@ -0,0 +1,31 @@
+def get_hold_release_times(total_time):
+    for hold in range(1, total_time):
+        yield hold, (total_time - hold)
+
+
+def task1(filename):
+    data = {}
+    with open(filename) as file:
+        for line in file:
+            name, values = line.split(":")
+            data[name] = map(int, values.strip().split())
+    races = zip(data["Time"], data["Distance"])
+    total_result = 1
+    for time, distance in races:
+        wins = 0
+        for hold, release in get_hold_release_times(time):
+            if (hold * release) > distance:
+                wins += 1
+        total_result *= wins
+    return total_result
+
+
+def task2(filename):
+    with open(filename) as file:
+        ...
+
+
+if __name__ == "__main__":
+    filename = "inputs/day6.txt"
+    print("Task 1:", task1(filename))
+    # print("Task 2:", task2(filename))
