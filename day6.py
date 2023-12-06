c6657e21472c05d3dba168f660fbe6b0b7c29b42
dkhex <dk-hex@yandex.ru>
2023-12-06 21:23:45 +0300

    Day 6 task 2 solution, fucking bruteforce

--- day6.py
+++ day6.py
@@ -21,11 +21,21 @@ def task1(filename):
 
 
 def task2(filename):
+    data = {}
     with open(filename) as file:
-        ...
+        for line in file:
+            name, values = line.split(":")
+            data[name] = int("".join(values.strip().split()))
+
+    time, distance = data["Time"], data["Distance"]
+    wins = 0
+    for hold, release in get_hold_release_times(time):
+        if (hold * release) > distance:
+            wins += 1
+    return wins
 
 
 if __name__ == "__main__":
     filename = "inputs/day6.txt"
     print("Task 1:", task1(filename))
-    # print("Task 2:", task2(filename))
+    print("Task 2:", task2(filename))
