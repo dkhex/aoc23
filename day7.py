d79448515fd30cf02d1ee5a727367dfda901fd48
dkhex <dk-hex@yandex.ru>
2023-12-07 20:54:26 +0300

    Day 7 task 2 solution, pretty ok

--- day7.py
+++ day7.py
@@ -3,7 +3,7 @@ from collections import Counter
 from myiters import ChainIterable
 
 
-strengths = {
+strengths_base = {
     "A": 14,
     "K": 13,
     "Q": 12,
@@ -20,7 +20,25 @@ strengths = {
 }
 
 
+strengths_joker = {
+    "A": 14,
+    "K": 13,
+    "Q": 12,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "J": 0,
+}
+
+
 powers = {
+    0: lambda _: 7,
     1: lambda _: 7,
     2: lambda c: 6 if c == 4 else 5,
     3: lambda c: 4 if c == 3 else 3,
@@ -34,13 +52,22 @@ def get_power(cards):
     return powers[len(kinds)](max(kinds.values()))
 
 
-def get_strength(hand):
-    return map(strengths.__getitem__, hand)
+def get_power_with_joker(cards):
+    kinds = Counter(filter(bool, cards))
+    power_func = powers[len(kinds)]
+    counts = kinds.values() or [0]
+    max_count = max(counts) + cards.count(0)
+    return power_func(max_count)
 
 
 def process_game(hand, bid):
-    cards = [strengths[card] for card in hand]
+    cards = [strengths_base[card] for card in hand]
     return get_power(cards), cards, int(bid)
+
+
+def process_game_with_joker(hand, bid):
+    cards = [strengths_joker[card] for card in hand]
+    return get_power_with_joker(cards), cards, int(bid)
 
 
 def task1(filename):
@@ -58,10 +85,18 @@ def task1(filename):
 
 def task2(filename):
     with open(filename) as file:
-        ...
+        return (
+            ChainIterable(file)
+                .map(str.split)
+                .tuple_map(process_game_with_joker)
+                .sort()
+                .enumerate(1)
+                .tuple_map(lambda rank, game: rank * game[2])
+                .sum()
+        )
 
 
 if __name__ == "__main__":
     filename = "inputs/day7.txt"
     print("Task 1:", task1(filename))
-    # print("Task 2:", task2(filename))
+    print("Task 2:", task2(filename))
