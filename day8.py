00f88fae4a4097c488f9e11a4040666053ec856c
dkhex <dk-hex@yandex.ru>
2023-12-08 18:31:37 +0300

    Day 8 task 2 solution, too lazy to refactor

--- day8.py
+++ day8.py
@@ -1,4 +1,5 @@
 from itertools import cycle
+from math import lcm
 
 
 directions = {
@@ -28,10 +29,32 @@ def task1(filename):
 
 
 def task2(filename):
+    points = []
+    nodes = {}
     with open(filename) as file:
-        ...
+        way = next(file).strip()
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            name, pair = line.split(" = ")
+            left, right = pair.strip("()").split(", ")
+            nodes[name] = (left, right)
+            if name.endswith("A"):
+                points.append(name)
+
+    steps = []
+    for step, direction in enumerate(cycle(way)):
+        if any(point.endswith("Z") for point in points):
+            steps.append(step)
+        points = [nodes[point][directions[direction]] for point in points if not point.endswith("Z")]
+        if not points:
+            break
+
+    return lcm(*steps)
+
 
 if __name__ == "__main__":
     filename = "inputs/day8.txt"
     print("Task 1:", task1(filename))
-    # print("Task 2:", task2(filename))
+    print("Task 2:", task2(filename))
