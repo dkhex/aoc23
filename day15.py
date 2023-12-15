1a49e1438ae040fc60e20c1ff3cb37d1b684e3d3
Vladislav Tretiakov <v.tretiakov@mirtekgroup.ru>
2023-12-15 11:18:25 +0300

    Day 15 task 2 solution, thanks python for preserving dict insertion order

--- day15.py
+++ day15.py
@@ -1,25 +1,47 @@
+def get_hash(label):
+    label_hash = 0
+    for char in label:
+        label_hash += ord(char)
+        label_hash *= 17
+    label_hash %= 256
+    return label_hash
+
+
+def get_boxes_powers(boxes):
+    for box_number, lenses in enumerate(boxes, start=1):
+        for lens_num, focal in enumerate(lenses.values(), start=1):
+            yield box_number * lens_num * focal
+
+
 def task1(filename):
     with open(filename) as file:
         sequence = file.read().strip().split(",")
 
-    hash_sum = 0
-    for step in sequence:
-        step_hash = 0
-        for char in step:
-            step_hash += ord(char)
-            step_hash *= 17
-        step_hash %= 256
-        hash_sum += step_hash
-
-    return hash_sum
+    return sum(map(get_hash, sequence))
 
 
 def task2(filename):
     with open(filename) as file:
-        ...
+        sequence = file.read().strip().split(",")
+
+    boxes = [{} for _ in range(256)]
+
+    for step in sequence:
+        if "-" in step:
+            label = step[:step.find("-")]
+            label_hash = get_hash(label)
+            if label in boxes[label_hash]:
+                del boxes[label_hash][label]
+        elif "=" in step:
+            label = step[:step.find("=")]
+            label_hash = get_hash(label)
+            value = int(step[-1])
+            boxes[label_hash][label] = value
+
+    return sum(get_boxes_powers(boxes))
 
 
 if __name__ == "__main__":
     filename = "inputs/day15.txt"
     print("Task 1:", task1(filename))
-    # print("Task 2:", task2(filename))
+    print("Task 2:", task2(filename))
